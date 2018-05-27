969d453b04c412d3d8190c6afaa53095ae1155da
freddydk <freddy.kristiansen@microsoft.com>
2018-05-26 22:47:03 -0700

    error handling

--- SetupCertificate.ps1
+++ SetupCertificate.ps1
@@ -1,50 +1,55 @@
 $ContactEMailForLetsEncrypt = $env:ContactEMailForLetsEncrypt
 
-mkdir c:\inetpub\wwwroot\http | Out-Null
-new-website -name http -port 80 -physicalpath c:\inetpub\wwwroot\http | Out-Null
-
-Write-Host "Installing NuGet PackageProvider"
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -ErrorAction Ignore | Out-Null
-
-Write-Host "Installing ACMESharp PowerShell modules"
-Install-Module -Name ACMESharp -AllowClobber -force -ErrorAction Ignore | Out-Null
-Install-Module -Name ACMESharp.Providers.IIS -force -ErrorAction Ignore | Out-Null
-Import-Module ACMESharp
-Enable-ACMEExtensionModule -ModuleName ACMESharp.Providers.IIS
-
-Write-Host "Initializing ACMEVault"
-Initialize-ACMEVault
-            
-Write-Host "Register Contact EMail address and accept Terms Of Service"
-New-ACMERegistration -Contacts "mailto:$ContactEMailForLetsEncrypt" -AcceptTos
-            
-Write-Host "Creating new dns Identifier"
-$dnsAlias = "dnsAlias"
-New-ACMEIdentifier -Dns $publicDnsName -Alias $dnsAlias
-
-Write-Host "Performing Lets Encrypt challenge to default web site"
-Complete-ACMEChallenge -IdentifierRef $dnsAlias -ChallengeType http-01 -Handler iis -HandlerParameters @{ WebSiteRef = 'http' }
-Submit-ACMEChallenge -IdentifierRef $dnsAlias -ChallengeType http-01
-sleep -s 60
-Update-ACMEIdentifier -IdentifierRef $dnsAlias
-
-Write-Host "Requesting certificate"
-$certAlias = "certAlias"
-$certificatePfxPassword = [GUID]::NewGuid().ToString()
-$certificatePfxFile = Join-Path $runPath "certificate.pfx"
-New-ACMECertificate -Generate -IdentifierRef $dnsAlias -Alias $certAlias
-Submit-ACMECertificate -CertificateRef $certAlias
-Update-ACMECertificate -CertificateRef $certAlias
-Get-ACMECertificate -CertificateRef $certAlias -ExportPkcs12 $certificatePfxFile -CertificatePassword $certificatePfxPassword
-
-$certificatePemFile = Join-Path $runPath "certificate.pem"
-Remove-Item -Path $certificatePemFile -Force -ErrorAction Ignore
-Get-ACMECertificate -CertificateRef $certAlias -ExportKeyPEM $certificatePemFile
-
-$cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($certificatePfxFile, $certificatePfxPassword)
-$certificateThumbprint = $cert.Thumbprint
-
-$dnsidentity = $cert.GetNameInfo("SimpleName",$false)
-if ($dnsidentity.StartsWith("*")) {
-    $dnsidentity = $dnsidentity.Substring($dnsidentity.IndexOf(".")+1)
+try {
+    mkdir c:\inetpub\wwwroot\http | Out-Null
+    new-website -name http -port 80 -physicalpath c:\inetpub\wwwroot\http | Out-Null
+    
+    Write-Host "Installing NuGet PackageProvider"
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -ErrorAction Ignore | Out-Null
+    
+    Write-Host "Installing ACMESharp PowerShell modules"
+    Install-Module -Name ACMESharp -AllowClobber -force -ErrorAction Ignore | Out-Null
+    Install-Module -Name ACMESharp.Providers.IIS -force -ErrorAction Ignore | Out-Null
+    Import-Module ACMESharp
+    Enable-ACMEExtensionModule -ModuleName ACMESharp.Providers.IIS | Out-Null
+    Write-Host "Initializing ACMEVault"
+    Initialize-ACMEVault
+                
+    Write-Host "Register Contact EMail address and accept Terms Of Service"
+    New-ACMERegistration -Contacts "mailto:$ContactEMailForLetsEncrypt" -AcceptTos | Out-Null
+                
+    Write-Host "Creating new dns Identifier"
+    $dnsAlias = "dnsAlias"
+    New-ACMEIdentifier -Dns $publicDnsName -Alias $dnsAlias | Out-Null
+    
+    Write-Host "Performing Lets Encrypt challenge to default web site"
+    Complete-ACMEChallenge -IdentifierRef $dnsAlias -ChallengeType http-01 -Handler iis -HandlerParameters @{ WebSiteRef = 'http' } | Out-Null
+    Submit-ACMEChallenge -IdentifierRef $dnsAlias -ChallengeType http-01 | Out-Null
+    sleep -s 60
+    Update-ACMEIdentifier -IdentifierRef $dnsAlias | Out-Null
+    
+    Write-Host "Requesting certificate"
+    $certAlias = "certAlias"
+    $certificatePfxPassword = [GUID]::NewGuid().ToString()
+    $certificatePfxFile = Join-Path $runPath "certificate.pfx"
+    New-ACMECertificate -Generate -IdentifierRef $dnsAlias -Alias $certAlias | Out-Null
+    Submit-ACMECertificate -CertificateRef $certAlias | Out-Null
+    Update-ACMECertificate -CertificateRef $certAlias | Out-Null
+    Get-ACMECertificate -CertificateRef $certAlias -ExportPkcs12 $certificatePfxFile -CertificatePassword $certificatePfxPassword | Out-Null
+    
+    $certificatePemFile = Join-Path $runPath "certificate.pem"
+    Remove-Item -Path $certificatePemFile -Force -ErrorAction Ignore | Out-Null
+    Get-ACMECertificate -CertificateRef $certAlias -ExportKeyPEM $certificatePemFile | Out-Null
+    
+    $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($certificatePfxFile, $certificatePfxPassword)
+    $certificateThumbprint = $cert.Thumbprint
+    
+    $dnsidentity = $cert.GetNameInfo("SimpleName",$false)
+    if ($dnsidentity.StartsWith("*")) {
+        $dnsidentity = $dnsidentity.Substring($dnsidentity.IndexOf(".")+1)
+    }
+}
+catch {
+    # If Any error occurs (f.ex. rate-limits), setup self signed certificate
+    . (Join-Path $runPath $MyInvocation.MyCommand.Name)
 }
